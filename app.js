e2471e514c9930eef18b71d5b22da6f60944db35
SeveSar <72022012+SeveSar@users.noreply.github.com>
2021-03-21 13:17:58 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const App = {
             myPlaseholder: "Пиши заметку",
             myInputValue: "",
             notes: [],
-            readyNotes: 0
-
+            readyNotes: [],
+            readyNotesCounter: 0,
         }
     },
     methods: {
@@ -18,21 +18,31 @@ const App = {
         addNewNotes(event) {
             if (this.myInputValue != "") {
                 this.notes.push(this.myInputValue);
+                localStorage.setItem('notes', JSON.stringify(this.notes));
                 this.myInputValue = "";
             }
         },
         deleteNote(index,event) {
             this.notes.splice(index,1);
-            if (this.readyNotes > 0) {
-                this.readyNotes -=1;
+            localStorage.setItem('notes', JSON.stringify(this.notes));
+            if (this.readyNotesCounter > 0) {
+                this.readyNotesCounter -=1;
             }
         },
-        readyNote(index,event) {
-            event.target.closest('li').classList.add("ready-note");
-            console.log(event.target);
-            this.readyNotes +=1;
-            this.$refs.list.append(event.target.closest('li'));
-            event.target.remove();
+        deleteReadyNote(index) {
+            this.readyNotes.splice(index,1);
+            localStorage.setItem('notesReady', JSON.stringify(this.readyNotes));
+        },
+        readyNote(myNote,event) {
+            // event.target.closest('li').classList.add("ready-note");
+            // this.readyNotesCounter +=1;
+            // this.$refs.list.append(event.target.closest('li'));
+            // event.target.remove();
+            // localStorage.setItem('notesReady', JSON.stringify(this.notes));
+            this.readyNotes.push(myNote);
+            localStorage.setItem('notesReady', JSON.stringify(this.readyNotes));
+            console.log(this.readyNotes);
+
         },
         toUpperCase(item) {
             return item.toUpperCase();
@@ -42,6 +52,14 @@ const App = {
         //         this.addNewNotes()
         //     }
         // }
+    },
+    mounted() {
+        if (localStorage.getItem('notes')) {
+           this.notes = JSON.parse(localStorage.getItem('notes'));
+        }
+        if (localStorage.getItem('notesReady')) {
+            this.readyNotes = JSON.parse(localStorage.getItem('notesReady'));
+        }
     },
     computed: {
         // Компьтед отимизирует код, если есть какието переменные внутри нашего приложения от которого мы вычесляем результат, в нашем случает этот результат зависит от this.notes то кидаем в Computed(они геттеры, сеттеры НЕ вызываем ())
@@ -57,9 +75,9 @@ const App = {
             if (value.length > 10) {
                 this.myInputValue = ""
             }
-        }
+        },
+
     }
 }
-
 Vue.createApp(App).mount('#app');
 
